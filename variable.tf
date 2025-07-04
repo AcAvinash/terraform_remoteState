5e37cb50d2cde2a7917aec254d7da283b585e65f
AVINASH KUMAR SINGH <109014651+AcAvinash@users.noreply.github.com>
2025-07-04 18:19:13 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,39 @@
+
+
+variable "ingress"{
+  description = "List of ingress rules for the security group"
+  type= list
+  default=[
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "egress" {
+  description = "List of egress rules for the security group"
+  type = list
+  default = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+}
